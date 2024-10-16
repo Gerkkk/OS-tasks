c1a5f2a3e5f0eee1e3ac498ee65915158703963f
Danila <danpol2005@gmail.com>
2024-10-16 12:18:26 +0300

    added sems 1-6

---
+++ README.md
@@ -0,0 +1,10 @@
+# OS Tasks
+
+Here are tasks that I solved during OS course in HSE. 
+
+1) seminar1 - Introduction, user id, group id;
+2) seminar2 - fork;
+3) seminar3 - execl, environment variables;
+4) seminar4 - sys calls for reading and writing to files. Communication between processes using pipe;
+5) seminar5 - communication between processes using FIFO;
+6) seminar6 - shared memory.
--- seminar5/task1/reader.c
+++ seminar5/task1/reader.c
@@ -6,22 +6,6 @@
 #include <stdlib.h>
 #include <errno.h>
 
-all: writer reader
-
-reader.o: reader.c
-	gcc -O0 -g3 -Wall -c reader.c
-	gcc -masm=intel -g3 -Wall -c reader.c -S
-writer.o: writer.c
-	gcc -O0 -g3 -Wall -c writer.c
-	gcc -masm=intel -g3 -Wall -c writer.c -S
-reader: reader.o
-	gcc -o reader reader.o
-
-writer: writer.o
-	gcc -o writer writer.o
-clean:
-	rm -rf *.o aaa.fifo  writer reader *.s
-
 int main()
 {
   int     fd;
@@ -57,50 +41,3 @@ int main()
     }  
   return 0;
 }  
-
-
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-
-int main()
-{
-  int     fd;
-  size_t  size;
-  char    name[] = "aaa.fifo";
-
-  (void)umask(0);
-
-  //if (mknod(name, S_IFIFO | 0666, 0) < 0) {                                   
-  if (mkfifo(name, 0666) < 0 && errno != EEXIST ) {
-      printf("Can\'t create FIFO, by another reason\n");
-      exit(-1);
-  }
-  
-  if ((fd = open(name, O_WRONLY, O_NDELAY)) < 0) {
-      printf("Can\'t open FIFO for writting\n");
-      exit(-1);
-  }
-  
-  size = write(fd, "Hello, world!", 14);
-
-  if (size != 14) {
-      printf("Can\'t write all string to FIFO\n");
-      exit(-1);
-  }
-
-
-  if (close(fd) < 0) {
-      printf("parent: Can\'t close FIFO\n"); exit(-1);
-  }
-
-  printf("Writer exit\n");
-
-  
-  return 0;
-}
